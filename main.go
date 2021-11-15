07eccaba8c6f7d9235cb11640ab1ba8a9709e46a
Edwing123 <egarciavalle2014@gmail.com>
2021-11-15 00:58:03 -0600

    feat: setup HTTP server with handlers
    Setup HTTP server with the following handlers:
    - Index handler
    - Static assets handler

--- main.go
+++ main.go
@@ -1,3 +1,31 @@
 package main
 
-func main() {}
+import (
+	"log"
+	"net/http"
+
+	"github.com/Edwing123/todo-app-with-authentication/env"
+	"github.com/Edwing123/todo-app-with-authentication/handlers"
+	"github.com/Edwing123/todo-app-with-authentication/views"
+)
+
+func main() {
+	views := views.CreateManager(templates)
+	env := env.CreateEnv(views, nil)
+	handlersFuncMap := handlers.CreateHandlerFuncsMap(env)
+	serveMux := http.NewServeMux()
+
+	// register handler functions
+	for route, handlerFunc := range handlersFuncMap {
+		serveMux.HandleFunc(route, handlerFunc)
+	}
+
+	// Set route for static assets
+	serveMux.Handle("/assets/", createAssetsHandler())
+
+	// run server
+	addr := ":8080"
+
+	log.Printf("Running on %s", addr)
+	log.Fatalln(http.ListenAndServe(addr, serveMux))
+}
